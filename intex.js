e9c0c105463943ddc4956810dec0febe782cb564
DaniMasterLucky <danielsilvaloew@gmail.com>
2023-04-27 21:30:55 -0300

    Strings & arrays

---
+++ intex.js
@@ -0,0 +1,28 @@
+/*Const nome = prompt("Digite seu nome")
+nome.length === 0 ?
+console.log ("Você precisa digitar algo."):
+console.log("o nome é",nome)*/ 
+
+// const sobrenome = "ZukenBerg"
+// console.log(sobrenome.toLowerCase())
+// console.log(sobrenome.toUpperCase())
+
+// const nome = "Maria antonieta"
+// console.log(nome.trim())
+
+
+
+// const receita = "Bolo de abacaxi"
+// const NovaReceita = receita.replace("abacaxi","cenoura")
+// console.log(NovaReceita)
+// console.log(receita.trim().toLowerCase().includes("abacaxi")); 
+
+
+const frase = prompt("Por favor, escreva qualquer frase.")
+const frasemaiusculo = frase.toLowerCase()
+console.log(frasemaiusculo)
+const NovaFrase = frasemaiusculo.replace("o","i")
+console.log(NovaFrase);
+console.log(NovaFrase.length);
+
+
